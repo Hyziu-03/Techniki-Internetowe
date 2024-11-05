230a4e19912bcc68023a700cde625f0ff0e2f941
Szymon Hyziak <szymon.hyziak@protonmail.com>
2024-11-05 14:07:34 +0100

    Dodanie mechaniki do przycisku z kodem

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Fotograf w Krakowie
 
 - Przekonwertować zdjęcia na format .webp
-- Dodać JavaScript do przycisku z kodem
+- Naprawić strzałkę na stronie z ofertą
--- index.js
+++ index.js
@@ -14,3 +14,10 @@ function awaitScroll(selector, coord) {
         behavior: "smooth",
     }))
 }
+
+const contentCopyBtn = document.querySelector(".content_copy");
+const contentCopyIcon = document.querySelector(".content_copy_icon");
+contentCopyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText("PIERWSZASESJA")
+    contentCopyIcon.innerText = "check"
+})
